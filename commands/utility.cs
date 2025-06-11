40e4112448c23088b910cab189704a26b93aac8a
KeDeX <albers.mark02@gmail.com>
2025-06-11 19:13:50 +0200

    add reactMessage method for role management

--- commands/utility.cs
+++ commands/utility.cs
@@ -33,5 +33,49 @@ namespace discord_kdx_bot.commands
                 new DiscordInteractionResponseBuilder().AddEmbed(embed));
         }
 
+        [SlashCommand("addreactionmessage", "Adds a reaction message")]
+        public async Task ReactionMessage(
+            InteractionContext ctx,
+            [Option("message_link", "Link to the target message")] string message_link, 
+            [Option("role", "select the roles as a list")] string? Role = null)
+        {
+            if (Role == null)
+            {
+                var botMember = await ctx.Guild.GetMemberAsync(ctx.Client.CurrentUser.Id);
+                var botRole = botMember.Roles.OrderByDescending(r => r.Position).FirstOrDefault();
+
+                var roles = ctx.Guild.Roles.Values
+                    .Where(role =>
+                    !role.IsManaged &&
+                    role.Name != "@everyone" &&
+                    role.Position < botRole.Position)
+                    .OrderByDescending(r => r.Position)
+                    .ToList();
+
+                var embed = new DiscordEmbedBuilder()
+                {
+                    Title = "Select Role",
+                    Description = "Select the Role you want to give, when someone reacts to the message",
+                    Color = DiscordColor.Red
+                };
+
+                var role_arr = roles
+                    .Select(r => new DiscordSelectComponentOption(r.Name, r.Name))
+                    .ToList();
+
+                var dropDownMenu = new DiscordSelectComponent(
+                    customId: "role_drop_down",
+                    placeholder: "Select the role",
+                    options: role_arr
+                );
+
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder()
+                .AddEmbed(embed)
+                .AddComponents(dropDownMenu));
+
+            }
+        }
+
     }
 }
