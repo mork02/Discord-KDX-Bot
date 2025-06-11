03408642b2cc70e6316ae97202df41e9ebdb2b78
KeDeX <albers.mark02@gmail.com>
2025-06-11 13:49:58 +0200

    add ping util and dice prefix

---
+++ commands/prefix.cs
@@ -0,0 +1,16 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace discord_kdx_bot.commands
+{
+    public class prefixCommands : BaseCommandModule
+    {
+        [Command("dice")]
+        public async Task DiceCommand(CommandContext ctx, int length = 6)
+        {
+            Random rnd = new Random();
+            int diceNum = rnd.Next(1, length + 1);
+            await ctx.Channel.SendMessageAsync("You rolled: " + diceNum.ToString());
+        }
+    }
+}
---
+++ commands/utility.cs
@@ -0,0 +1,37 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+namespace discord_kdx_bot.commands
+{
+    public class utilityCommands : ApplicationCommandModule
+    {
+        [SlashCommand("activedevbadge", "Shows you how to get the Badge.")]
+        public async Task ActiveDevBadgeCommand(InteractionContext ctx)
+        {
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Active Developer Badge",
+                Description = "[Click here, to get the Badge](https://discord.com/developers/active-developer)",
+                Color = DiscordColor.Red
+            };
+
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder().AddEmbed(embed));
+        }
+
+        [SlashCommand("ping", "Pings to check if the Bot is running.")]
+        public async Task PingCommand(InteractionContext ctx)
+        {
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Ping",
+                Description = "*Pong*",
+                Color = DiscordColor.Red
+            };
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder().AddEmbed(embed));
+        }
+
+    }
+}
