00642cd8a5f90fe350de5a2ebebd15a9a8f7b457
KeDeX <albers.mark02@gmail.com>
2025-06-16 20:27:18 +0200

    renamed file (only handle startup config)

--- config/configManager.cs
+++ config/configManager.cs
@@ -2,24 +2,24 @@
 
 namespace discord_kdx_bot.config
 {
-    internal class JSONReader
+    internal class ConfigManager
     {
         public string token { get; set; }
         public string prefix { get; set; }
 
-        public async Task ReadJSON()
+        public async Task Load()
         {
             using (StreamReader sr = new StreamReader("config.json"))
             {
                 string json = await sr.ReadToEndAsync();
-                JSONStructure data = JsonConvert.DeserializeObject<JSONStructure>(json);
+                BotConfig data = JsonConvert.DeserializeObject<BotConfig>(json);
                 token = data.token;
                 prefix = data.prefix;
             }
         }
 
     }
-    internal sealed class JSONStructure
+    internal sealed class BotConfig
     {
         public string token { get; set; }
         public string prefix { get; set; }
